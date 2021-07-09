c466bec3a37da11eed2cd6006227cb220fec1de1
Shivesh27 <ssshivesh@gmail.com>
2021-07-09 10:50:42 +0530

    Completed Login functionality

--- Main/models.py
+++ Main/models.py
@@ -4,8 +4,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class CustomUser(AbstractUser):
-	user_data = ((1,"admin"),(2,"staff"),(3,"student"))
-	user_type = models.CharField(default=1,choices=user_data,max_length=10)
+	user_data = (("1","admin"),("2","staff"),("3","student"))
+	user_type = models.CharField(default="1",choices=user_data,max_length=10)
 
 class adminUser(models.Model):
 	user_id = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
@@ -28,18 +28,18 @@ class Student(models.Model):
 @receiver(post_save,sender=CustomUser)
 def create_user_profile(sender,instance,created,**kwargs):
 	if created:
-		if instance.user_type==1:
+		if instance.user_type== "1":
 			adminUser.objects.create(user_id=instance)
-		if instance.user_type==2:
+		if instance.user_type== "2":
 			Teacher.objects.create(user_id=instance)
-		if instance.user_type==3:
+		if instance.user_type== "3":
 			Student.objects.create(user_id=instance)
 
 @receiver(post_save,sender=CustomUser)
 def save_user_profile(sender,instance,**kwargs):
-	if instance.user_type == 1:
+	if instance.user_type == "1":
 		instance.adminuser.save()
-	if instance.user_type == 2:
+	if instance.user_type == "2":
 		instance.teacher.save()
-	if instance.user_type == 3:
+	if instance.user_type == "3":
 		instance.student.save()
